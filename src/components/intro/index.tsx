1844102018af59fdbcdf481497c7385625d140e7
“Giovanny <giovanny.albarracin@swift-nav.com>
2024-10-19 20:18:19 -0500

    try

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -4,7 +4,7 @@ import "./index.css"
 function Intro() {
     return (
         <article>
-            <h1 className="title">Our expertise, your business</h1>
+            <p className="not-title">Our expertise, your business</p>
             <p className="text"> Our talented team of engineers has mastered advanced technologies in key areas and thrives on driving innovation with forward-looking companies.</p>
             <section className="section-container">
             { expertiseData.map((pic) => (
